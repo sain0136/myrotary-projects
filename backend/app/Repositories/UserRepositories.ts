381c3e6215598ca4fbf47ab09965bef6df6a4d1d
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-10-03 18:54:36 -0400

    feat: form switched issue so this is actual api calls for user created backend

--- backend/app/Repositories/UserRepositories.ts
+++ backend/app/Repositories/UserRepositories.ts
@@ -2,7 +2,9 @@ import Hash from "@ioc:Adonis/Core/Hash";
 import CustomException from "App/Exceptions/CustomException";
 import Clubs from "App/Models/Clubs";
 import Districts from "App/Models/Districts";
+import Projects from "App/Models/Projects";
 import Users from "App/Models/Users";
+import { IUser } from "App/Shared/Interfaces/IUser";
 import { errorTranslations } from "App/Translations/Translations";
 import { AuthenticationRequestData } from "App/Utils/CommonTypes";
 
@@ -76,5 +78,107 @@ export default class UserRepositories {
       district,
       club,
     };
+  }
+
+  public async store(user: IUser) {
+    await Users.create({
+      firstname: user.firstname,
+      lastname: user.lastname,
+      address: user.address,
+      userCity: user.user_city,
+      userPostal: user.user_postal,
+      userProvince: user.user_province,
+      userCountry: user.user_country,
+      phone: user.phone,
+      email: user.email,
+      password: user.password,
+      clubId: user.club_id,
+      districtId: user.district_id ? user.district_id : undefined,
+      userType: user.user_type,
+      extraDetails: JSON.stringify(user.extra_details),
+    });
+    if (user.user_type === "DISTRICT") {
+      const district = await Districts.findOrFail(user.district_id);
+      await user.related("districtRole").attach({
+        [district.districtId]: {
+          district_role: user.role_type,
+        },
+      });
+    } else {
+      const club = await Clubs.findOrFail(user.club_id);
+      await user.related("clubRole").attach({
+        [club.clubId]: {
+          club_role: user.role_type,
+        },
+      });
+    }
+  }
+
+  public async updateUser(user: IUser) {
+    const existingUser = await Users.findOrFail(user.user_id);
+    const updatedUser = await existingUser
+      .merge({
+        firstname: user.firstname,
+        lastname: user.lastname,
+        address: user.address,
+        userCity: user.user_city,
+        userPostal: user.user_postal,
+        userProvince: user.user_province,
+        userCountry: user.user_country,
+        phone: user.phone,
+        email: user.email,
+        password: user.password,
+        clubId: user.club_id,
+        districtId: user.district_id ? user.district_id : undefined,
+        userType: user.user_type,
+        extraDetails: JSON.stringify(user.extra_details),
+      })
+      .save();
+    if (user.user_type === "DISTRICT") {
+      await updatedUser.related("districtRole").detach();
+      const district: Districts = await Districts.findOrFail(user.district_id);
+      await updatedUser.related("districtRole").attach({
+        [district.districtId]: {
+          district_role: user.role_type,
+        },
+      });
+    } else {
+      await updatedUser.related("clubRole").detach();
+      const club: Clubs = await Clubs.findOrFail(user.club_id);
+      await updatedUser.related("clubRole").attach({
+        [club.clubId]: {
+          club_role: user.role_type,
+        },
+      });
+    }
+  }
+
+  public async delete(userId: number) {
+    const user = await Users.findOrFail(userId);
+    // TODO : REfoctor Db query for speed in future
+    const projects = await Projects.all();
+    const found = projects.find((project) => {
+      if (project.createdBy === user.userId) {
+        return true;
+      }
+    });
+    if (!found) {
+      if (user.userType === "DISTRICT") {
+        await user.related("districtRole").detach();
+        await user.delete();
+      } else {
+        await user.related("clubRole").detach();
+        await user.delete();
+      }
+    } else {
+      throw new CustomException({
+        message: "Cannot delete user",
+        status: 400,
+        translatedMessage: {
+          en: "Cannot delete user, there are projects created by this user",
+          fr: "Impossible de supprimer l'utilisateur, il y a des projets créés par cet utilisateur",
+        },
+      });
+    }
   }
 }
