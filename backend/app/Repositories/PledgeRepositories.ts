7f7c839b39299b54c0538f64f2a871cfcfd1a287
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-10-28 22:42:03 -0400

    feat: differnt lib addeded and store method

--- backend/app/Repositories/PledgeRepositories.ts
+++ backend/app/Repositories/PledgeRepositories.ts
@@ -1,20 +1,27 @@
 import Database from "@ioc:Adonis/Lucid/Database";
 import Pledges from "App/Models/Pledges";
 import Projects from "App/Models/Projects";
+import Dinero from "dinero.js";
 import { IPledge } from "App/Shared/Interfaces/IProjects";
 import { ProjectStatus } from "App/Shared/Types/commonTypes";
 
 export default class PledgesRepositories {
   public async storePledge(pledge: IPledge) {
+    // Initiating a database transaction to ensure data integrity
     await Database.transaction(async (trx) => {
+      // Fetching the project associated with the pledge within the transaction
       const project = await Projects.query({ client: trx })
         .where("id", pledge.project_id)
         .firstOrFail();
+
+      // Preparing a new pledge instance
       const newPledge = new Pledges();
       newPledge.useTransaction(trx);
+
+      // Saving the pledge details to the database
       await newPledge
         .merge({
-          pledgeAmount: pledge.pledge_amount,
+          pledgeAmount: pledge.pledge_amount, // Storing pledge amount in cents
           firstname: pledge.firstname,
           lastname: pledge.lastname,
           email: pledge.email,
@@ -28,24 +35,34 @@ export default class PledgesRepositories {
         })
         .save();
 
+      // Initializing Dinero instances for precise monetary arithmetic
+      const anticipatedFunding = Dinero({ amount: project.anticipatedFunding }); // already in cents
+      const pledgeAmount = Dinero({ amount: pledge.pledge_amount }); // already in cents
+      const fundingGoal = Dinero({ amount: project.fundingGoal }); // already in cents
+
+      // Checking if the project is fully funded after this pledge
       let changeStatus: Boolean = false;
       if (
-        parseFloat(project.anticipatedFunding.toString()) +
-          parseFloat(pledge.pledge_amount.toString()) ===
-          parseFloat(project.fundingGoal.toString()) &&
+        anticipatedFunding.add(pledgeAmount).equalsTo(fundingGoal) &&
         project.projectStatus === ProjectStatus.LOOKINGFORFUNDING
       ) {
         changeStatus = true;
       }
 
+      // Calculating the new total pledges and anticipated funding values
+      const newTotalPledges = Dinero({ amount: project.totalPledges })
+        .add(pledgeAmount)
+        .getAmount();
+      const newAnticipatedFunding = anticipatedFunding
+        .add(pledgeAmount)
+        .getAmount();
+
+      // Updating the project details in the database
       project.useTransaction(trx);
       await project
         .merge({
-          anticipatedFunding:
-            parseFloat(project.anticipatedFunding.toString()) +
-            pledge.pledge_amount,
-          totalPledges:
-            parseFloat(project.totalPledges.toString()) + pledge.pledge_amount,
+          anticipatedFunding: newAnticipatedFunding,
+          totalPledges: newTotalPledges,
           projectStatus: changeStatus
             ? ProjectStatus.FULLYFUNDED
             : ProjectStatus.LOOKINGFORFUNDING,
