3b936e95acc396e4d97cf682916088f8e09a713b
sain0136 <55252053+sain0136@users.noreply.github.com>
2024-02-17 17:01:25 -0500

    Add dynamic height support to Hr component

--- frontend/src/components/hr/Hr.vue
+++ frontend/src/components/hr/Hr.vue
@@ -4,9 +4,22 @@ export default {
 };
 </script>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { heightInPixels } = defineProps<{
+  heightInPixels?: string;
+}>();
+</script>
+
 <template>
-  <hr class="h-px my-8 bg-gray-500 border-0" />
+  <hr
+    :class="[
+      {
+        'h-px': !heightInPixels,
+        [`h-${heightInPixels}`]: heightInPixels,
+      },
+    ]"
+    class="h-0.5 my-8 bg-gray-500 border-0"
+  />
 </template>
 
 <style lang="scss" scoped></style>
