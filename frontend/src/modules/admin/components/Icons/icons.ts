b9733f81577792cbf1f9d137deb1ed5017ec10fc
sain0136 <55252053+sain0136@users.noreply.github.com>
2025-01-31 18:41:37 -0500

    feat: Add analytics icon and update translations for Rotary Analytics page

---
+++ frontend/src/modules/admin/components/Icons/icons.ts
@@ -0,0 +1,4 @@
+const analyticsIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M2 6.634a4.634 4.634 0 1 1 9.268 0a4.634 4.634 0 0 1-9.268 0" clip-rule="evenodd"/><path fill="currentColor" fill-rule="evenodd" d="M12.732 17.366a4.634 4.634 0 1 1 9.269 0a4.634 4.634 0 0 1-9.269 0" clip-rule="evenodd" opacity="0.5"/><path fill="currentColor" d="M2 17.5c0-2.121 0-3.182.659-3.841S4.379 13 6.5 13s3.182 0 3.841.659S11 15.379 11 17.5s0 3.182-.659 3.841S8.621 22 6.5 22s-3.182 0-3.841-.659S2 19.621 2 17.5"/><path fill="currentColor" d="M13 6.5c0-2.121 0-3.182.659-3.841S15.379 2 17.5 2s3.182 0 3.841.659S22 4.379 22 6.5s0 3.182-.659 3.841S19.621 11 17.5 11s-3.182 0-3.841-.659S13 8.621 13 6.5" opacity="0.5"/></svg>`;
+
+export { analyticsIcon };
--- frontend/src/modules/admin/views/SuperAdmin.vue
+++ frontend/src/modules/admin/views/SuperAdmin.vue
@@ -8,13 +8,14 @@ export default {
 import { useLanguage } from "@/utils/languages/UseLanguage";
 import { onMounted, ref } from "vue";
 import { errorHandler } from "@/utils/composables/ErrorHandler";
+import { analyticsIcon } from "@/modules/admin/components/Icons/icons";
 
 /* Data */
 const { langTranslations } = useLanguage();
 const { handleError } = errorHandler();
-// const { exampleProperty } = defineProps<{
+// const { exampleProperty } = defineProps({
 //   exampleProperty: string;
-// }>();
+// });
 
 /* Hooks */
 onMounted(async () => {});
@@ -27,14 +28,27 @@ onMounted(async () => {});
     class="h-full justify-center text-center gap-4 flex items-center flex-col text-2xl font-bold"
   >
     <a
-      class="text-blue-600 hover:text-primary"
-      target="_blank"
-      href="https://analytics.myrotaryprojects.org"
-      >{{ langTranslations.adminDash.rotaryAnalticsLink }}</a
+      href="#"
+      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
+      <div class="icon flex justify-center mb-4" v-html="analyticsIcon"></div>
+      <a
+        class="text-blue-600 hover:text-primary mt-4"
+        target="_blank"
+        href="https://analytics.myrotaryprojects.org"
+        >{{ langTranslations.adminDash.rotaryAnalyticsLink }}</a
+      >
+      <p class="font-normal text-gray-700 mt-4 text-left">
+        {{ langTranslations.rotaryAnalyticsDescription }}
+      </p>
+    </a>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "@/assets/_variables.scss";
+
+.icon {
+  color: #2f4f4f;
+}
 </style>
