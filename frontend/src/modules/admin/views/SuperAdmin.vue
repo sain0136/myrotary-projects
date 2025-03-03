b13331cb3a43ad7235541419c153a6437528836a
sain0136 <55252053+sain0136@users.noreply.github.com>
2025-03-02 22:32:22 -0500

    refactor: Update SuperAdmin.vue layout for improved structure and styling

--- frontend/src/modules/admin/views/SuperAdmin.vue
+++ frontend/src/modules/admin/views/SuperAdmin.vue
@@ -27,9 +27,9 @@ onMounted(async () => {});
   <div
     class="h-full justify-center text-center gap-4 flex items-center flex-col text-2xl font-bold"
   >
-    <a
+    <div
       href="#"
-      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      class="p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100"
     >
       <div class="icon flex justify-center mb-4" v-html="analyticsIcon"></div>
       <a
@@ -38,10 +38,10 @@ onMounted(async () => {});
         href="https://analytics.myrotaryprojects.org"
         >{{ langTranslations.adminDash.rotaryAnalyticsLink }}</a
       >
-      <p class="font-normal text-gray-700 mt-4 text-left">
+      <p class="font-normal text-gray-700 mt-4">
         {{ langTranslations.rotaryAnalyticsDescription }}
       </p>
-    </a>
+    </div>
   </div>
 </template>
 
