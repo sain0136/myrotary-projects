efc2c30f8cbc58b157d68145745069182e30cb74
Sebastien <jssr26@gmail.com>
2025-04-27 15:30:52 -0400

    fix: Extend session expiration threshold from 30 minutes to 4 hours

--- backend/app/Middleware/Authorize.ts
+++ backend/app/Middleware/Authorize.ts
@@ -47,7 +47,7 @@ export default class Authorize {
           Number(userSession.lastActivityTimestamp)
         );
         const lastActivityTimestampThreshold = lastActivityTimestamp.plus({
-          minutes: 30,
+          hours: 4,
         });
         if (lastActivityTimestampThreshold < DateTime.now()) {
           await userSession.delete();
